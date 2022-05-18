a18104f81ed09280c02066075657b55666965780
paul <paul.lee@cloud-interactive.com>
2022-05-18 18:30:04 +0800

    webpack.prod debug

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,8 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
-module.exports =function(webpackEnv){
-return merge(common(webpackEnv), {
-  mode: 'production',
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: '/public/index.html',
-    }),
-  ]
-})
-} 
+module.exports = function (webpackEnv) {
+  return merge(common(webpackEnv), {
+    mode: 'production',
+  })
+}
